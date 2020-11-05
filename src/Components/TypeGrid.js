73ea3d6035c740677a292aab352d629ab231aba0
faezix <amirfaezansari@gmail.com>
2020-11-05 10:14:38 +0330

    Footer fixed

--- src/Components/TypeGrid.js
+++ src/Components/TypeGrid.js
@@ -2,41 +2,33 @@ import React from "react";
 import TypesData from "./type.js";
 
 export default function TypeGrid(props) {
-  return (
-    <div className="Grid">
-      {TypesData.map((a, key) => (
-        <div className="Type" key={key}>
-          <h1
-            className="TypeName"
-            style={{
-              color:
-                   props.type.includes(a.type)
-                    ? "red"
-                    : "black"
-                  ,
-                  fontSize:
-                   props.type.includes(a.type)
-                    ?"2.5em" 
-                    : "2.3em"
-
-            }}
-          >
-            {a.type}
-          </h1>
-          <h1 className="Type-Des">{a.typeDes}</h1>
-          <div className="CF-container">
-            {a.CF.map((b, key) => (
-              <div className="CF" key={key}>
-                <h2>{b}</h2>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-      <footer style={{color: 'red',
-    fontSize: '1rem'}}>
-        created by Faezix
-      </footer>
-    </div>
-  );
+	return (
+		<div className="Grid">
+			{TypesData.map((a, key) => (
+				<div className="Type" key={key}>
+					<h1
+						className="TypeName"
+						style={{
+							color: props.type.includes(a.type)
+								? "red"
+								: "black",
+							fontSize: props.type.includes(a.type)
+								? "2.5em"
+								: "2.3em",
+						}}
+					>
+						{a.type}
+					</h1>
+					<h1 className="Type-Des">{a.typeDes}</h1>
+					<div className="CF-container">
+						{a.CF.map((b, key) => (
+							<div className="CF" key={key}>
+								<h2>{b}</h2>
+							</div>
+						))}
+					</div>
+				</div>
+			))}
+		</div>
+	);
 }
--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,70 +1,92 @@
-import React, { useState } from "react";
-import  { Q } from "./type.js";
-import "../index.css"
+import React from "react";
+import { Q } from "./type.js";
+import "../index.css";
 
 const CF = ["Se-Ni", "Si-Ne", "Ti-Fe", "Te-Fi"];
 const IS = [
-  "Direct",
-  "Informative",
-  "Initiating",
-  "Responding",
-  "Control",
-  "Movement",
+	"Direct",
+	"Informative",
+	"Initiating",
+	"Responding",
+	"Control",
+	"Movement",
 ];
 const Temps = [
-  "Affiliative",
-  "Pragmatic",
-  "Concrete",
-  "Abstract",
-  "Systematic",
-  "Interest",
+	"Affiliative",
+	"Pragmatic",
+	"Concrete",
+	"Abstract",
+	"Systematic",
+	"Interest",
 ];
-const Quadra = Object.keys(Q).map((a) => a);
-
+const Quadra = Object.keys(Q).map(a => a);
 
 export function Side(props) {
-  const clickHandler = (event) => {
-    
-    props.addBtn(event.target);
-  }
-  return (
-    <>
-    <div className="section1">
-      <div className="CF-section">
-        {CF.map((a, key) => (
-          <button onClick={clickHandler}  
-          className="btn"
-          key={key}>
-            {a}
-          </button>
-        ))}
-      </div>
-      <div className="Quadra-section">
-        {Quadra.map((a, key) => (
-          <button className="btn" onClick={clickHandler} key={key}>
-            {a}
-          </button>
-        ))}
-      </div>
-    </div>
+	const clickHandler = event => {
+		props.addBtn(event.target);
+	};
+	return (
+		<>
+			<div className="section1">
+				<div className="CF-section">
+					{CF.map((a, key) => (
+						<button
+							onClick={clickHandler}
+							className="btn"
+							key={key}
+						>
+							{a}
+						</button>
+					))}
+				</div>
+				<div className="Quadra-section">
+					{Quadra.map((a, key) => (
+						<button
+							className="btn"
+							onClick={clickHandler}
+							key={key}
+						>
+							{a}
+						</button>
+					))}
+				</div>
+			</div>
 
-    <div className="section2">
-    <div className="IS-section">
-      {IS.map((a, key) => (
-        <button onClick={clickHandler} key={key} className="btn">
-          {a}
-        </button>
-      ))}
-    </div>
+			<div className="section2">
+				<div className="IS-section">
+					{IS.map((a, key) => (
+						<button
+							onClick={clickHandler}
+							key={key}
+							className="btn"
+						>
+							{a}
+						</button>
+					))}
+				</div>
 
-    <div className="Temp-section">
-      {Temps.map((a, key) => (
-        <button onClick={clickHandler} key={key} className="btn">
-          {a}
-        </button>
-      ))}
-    </div>
-  </div>
-  </>
-  );
+				<div className="Temp-section">
+					{Temps.map((a, key) => (
+						<button
+							onClick={clickHandler}
+							key={key}
+							className="btn"
+						>
+							{a}
+						</button>
+					))}
+				</div>
+			</div>
+			<footer
+				style={{
+					color: "#FF0050",
+					fontSize: "1rem",
+					fontFamily: "monospace",
+					fontWeight: "bold",
+				}}
+			>
+				created by Faezix
+			</footer>
+		</>
+	);
 }
